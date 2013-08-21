78da99e49a03e0ed062d83b3f04ffbb16bcb37ac
Wilfred Hughes <me@wilfred.me.uk>
2013-08-21 18:03:34 +0100

    adding a basic description

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-ez-query-replace
-================
+# ez-query-replace
 
-A helpful query-replace for Emacs
+ez-query-replace is a simple wrapper around `query-replace' that adds
+a default search term, and allows you to conveniently replay old
+replacements.
+
+## Changelog
+
+### v0.1
+
+Initial release.
--- ez-query-replace.el
+++ ez-query-replace.el
@@ -7,6 +7,12 @@
 ;; Version: 0.1
 ;; Package-Requires: ((dash "1.2.0"))
 
+;;; Commentary:
+
+;; ez-query-replace is a simple wrapper around `query-replace' that adds a
+;; default search term, and allows you to conveniently replay old
+;; replacements.
+
 ;;; Usage:
 
 ;; The ez-query-replace commands are autoloaded, so you don't need to
